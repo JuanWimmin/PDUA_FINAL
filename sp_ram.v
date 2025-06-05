7e0dc6d2465c32280cd87c85ed899aaeb38c5e02
Basteteos <juanpacomo@gmail.com>
2025-06-04 22:08:19 -0500

    Version_Final_PDUA
    Procesador PDUA funcional que implementa algoritmo de booth

---
+++ sp_ram.v
@@ -0,0 +1,26 @@
+module sp_ram #(parameter DATA_WIDTH = 8,
+								  ADDR_WIDTH = 8)
+(		input wire  wr_rdn,
+		input wire  clk,
+		input wire  [ADDR_WIDTH-1:0] addr,
+		input wire  [DATA_WIDTH-1:0] w_data,
+		output wire [DATA_WIDTH-1:0] r_data);
+	
+reg  [ADDR_WIDTH-1:0] addr_reg;	
+reg  [DATA_WIDTH-1:0] ram [2**ADDR_WIDTH-1:0];
+
+
+initial begin
+   $readmeb("ram_init.hex", ram); // archivo de programa en hexadecimal
+end
+//always @(posedge clk, addr, w_data) begin
+always @(posedge clk) begin
+	if (wr_rdn) begin
+		ram[addr] <= w_data;
+	end
+	addr_reg <= addr;
+end
+
+assign r_data = ram[addr_reg];
+
+endmodule 
